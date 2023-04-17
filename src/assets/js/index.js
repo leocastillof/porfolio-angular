da11c751f4e2cfd25d226e428a590a76a6d01c48
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-04-17 10:31:08 -0300

    Change 001
    Transferir los archivos, carpetas a la rama de GitHub

---
+++ src/assets/js/index.js
@@ -0,0 +1,31 @@
+
+
+
+const myFunc = (navCondition) => {
+    if (navCondition === 'open') {
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if (navCondition === 'close') {
+        SideNav.classList.remove('show-nav');
+        btnTimes.style.display = "none";
+        btnBars.style.display = "block";
+    }
+}
+
+
+// Part 1 javascript functionality ends here
+$(document).ready(function () {
+    if (!$("#myCanvas").tagcanvas({
+        textColour: "#08fdd8",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weight: true,
+    }, "tags")) {
+        // something went wrong hide the canvas container,
+        $("#myCanvasContainer");
+    }
+})
