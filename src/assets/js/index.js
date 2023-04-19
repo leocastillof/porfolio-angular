b0720f23f064872cd38dfbe5071406526c10cc7d
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-04-19 17:09:28 -0300

    Music found

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,31 @@
 
 
+// Play music functionality
+document.addEventListener('DOMContentLoaded', () => {
+    const soundCloud = document.querySelector(".sound-cloud");
+    const off = document.getElementById("off");
+    const on = document.getElementById("on");
+    const myAudio = document.getElementById("myAudio");
 
+    off.addEventListener("click", () => soundTrack("off"));
+    on.addEventListener("click", () => soundTrack("on"));
+
+    const soundTrack = (soundState) => {
+        if (soundState === "off") {
+            on.style.display = "block";
+            off.style.display = "none";
+            soundCloud.style.color = "#fdbf2d";
+            myAudio.play();
+        } else if (soundState === "on") {
+            on.style.display = "none";
+            off.style.display = "block";
+            soundCloud.style.color = "#a51515";
+            myAudio.pause();
+        }
+    };
+});
+
+// Navbar
 const myFunc = (navCondition) => {
     if (navCondition === 'open') {
         SideNav.classList.add('show-nav');
@@ -15,7 +40,7 @@ const myFunc = (navCondition) => {
 }
 
 
-// Part 1 javascript functionality ends here
+// Functionality ends here
 $(document).ready(function () {
     if (!$("#myCanvas").tagcanvas({
         textColour: "#08fdd8",
