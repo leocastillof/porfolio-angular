22739f74311d181296d176f96e7cf090cf5e523c
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-11 00:26:44 -0300

    Integration Back to Front

---
+++ src/app/model/jdw-dto.ts
@@ -0,0 +1,6 @@
+export class JdwDto {
+    token!: String;
+    type!: String;
+    username!: String;
+    authorities!: String[];
+}
---
+++ src/app/service/token.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUsername';
+const AUTHORITIES_KEY = 'AuthAuthorities';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+}
