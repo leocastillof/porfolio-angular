51acaf41d7ccf3aec9dba1df567529fea33dd189
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-11 04:48:10 -0300

    Create experiencie.ts

---
+++ src/app/model/experiencie.ts
@@ -0,0 +1,11 @@
+export class Experiencie {
+    id?: number;
+    nameE: string;
+    descriptionE: string;
+
+    constructor(nameE: string, descriptionE: string)
+    {
+        this.nameE = nameE;
+        this.descriptionE = descriptionE;
+    }
+}
