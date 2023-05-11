a6b83042e4e5939965d5a5bd29e120000f10533d
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-11 00:45:18 -0300

    Token - Auth - Logout

--- src/app/service/token.service.ts
+++ src/app/service/token.service.ts
@@ -8,6 +8,50 @@ const AUTHORITIES_KEY = 'AuthAuthorities';
   providedIn: 'root'
 })
 export class TokenService {
+  roles: Array<string> = [];
 
   constructor() { }
+
+  // Token
+  public setToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    return sessionStorage.getItem(TOKEN_KEY)!;
+  }
+  
+  // Username
+  public setUsername(username: string): void {
+    window.sessionStorage.removeItem(USERNAME_KEY);
+    window.sessionStorage.setItem(USERNAME_KEY, username);
+  }
+
+  public getUsername(): string {
+    return sessionStorage.getItem(USERNAME_KEY)!;
+  }
+
+  // Authorities
+  public setAuthorities(authorities: string[]): void {
+    window.sessionStorage.removeItem(AUTHORITIES_KEY);
+    window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+  }
+
+  public getAuthorities(): string[] {
+    this.roles = [];
+    if(sessionStorage.getItem(AUTHORITIES_KEY)!){
+      JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority:any) =>
+      {
+        this.roles.push(authority.authority);
+      });
+    }
+    return this.roles;
+  }
+
+  // Logout
+  public logout(): void{
+    window.sessionStorage.clear();
+  }
+
 }
