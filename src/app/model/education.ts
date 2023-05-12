bc4d5da846f874bbaa49a02381a315570d4a21f8
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-12 00:57:26 -0300

    Education Frontend

---
+++ src/app/model/education.ts
@@ -0,0 +1,11 @@
+export class Education {
+    id?: number;
+    nameE: string;
+    descriptionE: string;
+
+    constructor(nameE: string, descriptionE: string)
+    {
+        this.nameE = nameE;
+        this.descriptionE = this.descriptionE;
+    }
+}
